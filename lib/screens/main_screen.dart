bdaaf72987fea0e21fc8a26f8f90ea7d70285a0d
clicker <clickernux@gmail.com>
2023-10-04 22:09:52 +0630

    Implemented CustomScrollView in MainScreen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -5,8 +5,55 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Placeholder()),
+    return Scaffold(
+      body: _buildbody(context),
+    );
+  }
+
+  _buildbody(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Container(
+            height: 320,
+            // padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/repair_phone2.jpg'),
+                  fit: BoxFit.cover),
+            ),
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.3),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'ဝမ်းနိုင်းနိုင်း ဖုန်းပြုပြင်ရေး',
+                    style: textTheme.headlineSmall?.copyWith(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    'ကျွမ်းကျင်မှုဖြင့်သာ ဝန်ဆောင်မှုပေးသည်',
+                    style: textTheme.labelMedium?.copyWith(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
