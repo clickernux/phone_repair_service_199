1af16f7eed773aa3d098f178fc7eeb4ba35f7339
clicker <clickernux@gmail.com>
2023-10-05 20:48:57 +0630

    Removed SliverAppBar and used simple appbar instea

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:phone_repair_service_199/util.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
     return Scaffold(
+      appBar: AppBar(
+        title: Text(Util.appNameMM),
+        titleTextStyle: textTheme.labelLarge,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(LineIcons.bell),
+          ),
+        ],
+      ),
       body: _buildbody(context),
     );
   }
@@ -14,46 +27,7 @@ class MainScreen extends StatelessWidget {
     final TextTheme textTheme = Theme.of(context).textTheme;
 
     return CustomScrollView(
-      slivers: [
-        SliverToBoxAdapter(
-          child: Container(
-            height: 320,
-            // padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/images/repair_phone2.jpg'),
-                  fit: BoxFit.cover),
-            ),
-            alignment: Alignment.bottomLeft,
-            child: Container(
-              width: double.infinity,
-              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
-              decoration: BoxDecoration(
-                color: Colors.black.withOpacity(0.3),
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'ဝမ်းနိုင်းနိုင်း ဖုန်းပြုပြင်ရေး',
-                    style: textTheme.headlineSmall?.copyWith(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'ကျွမ်းကျင်မှုဖြင့်သာ ဝန်ဆောင်မှုပေးသည်',
-                    style: textTheme.labelMedium?.copyWith(color: Colors.white),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ],
+      slivers: [],
     );
   }
 }
