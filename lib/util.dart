abd200b34ce70914874b34f70c56b33959a5db4f
clicker <clickernux@gmail.com>
2023-10-09 20:28:48 +0630

    Created bannerContent list.

--- lib/util.dart
+++ lib/util.dart
@@ -1,5 +1,35 @@
 class Util {
   static String appNameMM = 'ဝမ်းနိုင်းနိုင်း ဖုန်းပြုပြင်ရေး';
-
   static String appNameEng = '199 Phone Repair Service';
+
+  static List<({String text, String imgPath})> bannerContent = [
+    (
+      text:
+          'နေ့စဉ် မရှိမဖြစ်အသုံးပြုနေရတဲ့ သင့်ဖုန်းမှာ ပြဿနာတချို့ရှိနေပြီလား?',
+      imgPath: 'assets/images/repair_phone.jpg',
+    ),
+    (
+      text: 'ဖုန်းရေဝင်ပြီးနောက် အားကုန်မြန်တာ၊ စက်ပူလာတာမျိုး ကြုံနေရပြီလား?',
+      imgPath: 'assets/images/repair_phone2.jpg',
+    ),
+    (
+      text: 'ဖုန်းလွတ်ကျထားလို့ မှန်အကွဲကြီးနဲ့ ရှိုးပဲ့နေပြီလား?',
+      imgPath: 'assets/images/repair_phone3.jpg',
+    ),
+    (
+      text:
+          'ကိုယ်ပြော သူမကြား၊ သူပြောကိုယ်မကြားနဲ့ အသံပိုင်းဆိုင်ရာ ပြဿနာရှိနေပြီလား?',
+      imgPath: 'assets/images/repair_phone4.webp',
+    ),
+    (
+      text:
+          'ဖုန်းလိုင်းမတက်၊ လိုင်းဆွဲအားနည်းနေလို့ အဆက်သွယ်တွေ ပျက်ကုန်ပြီလား?',
+      imgPath: 'assets/images/repair_phone5.webp',
+    ),
+    (
+      text:
+          'ယုံကြည်စိတ်ချရပြီး ကျွမ်းကျင် သက်သာ မြန်ဆန်တဲ့ ဖုန်းပြင်ဆိုင်မတွေ့လို့ သင့်ဖုန်းကို ပြင်ဖို့ တွန့်ဆုတ်နေပြီလား?',
+      imgPath: 'assets/images/repair_phone6.webp',
+    ),
+  ];
 }
