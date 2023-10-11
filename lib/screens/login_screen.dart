cdc5328cdfabc0698a57edf28a7d7fe7f66fc38b
clicker <clickernux@gmail.com>
2023-10-11 22:02:21 +0630

    Created login_screen.dart

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Admin Panel'),
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return const Placeholder();
+  }
+}
