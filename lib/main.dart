53f120f21648cb3e5c4ebe44300cb57cc85a250d
clicker <clickernux@gmail.com>
2023-10-11 22:04:09 +0630

    Added and implemented firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:phone_repair_service_199/firebase_options.dart';
 import 'package:phone_repair_service_199/main_router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const PhoneRepairService199App());
 }
 
