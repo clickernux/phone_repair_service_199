26374011d2b9f60273d2666924eaa7bce0a5fa5a
clicker <clickernux@gmail.com>
2023-10-11 22:03:34 +0630

    Implemented onLongPress method on appbar title.

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:phone_repair_service_199/components/component_layer.dart';
 import 'package:phone_repair_service_199/util.dart';
@@ -11,7 +12,10 @@ class MainScreen extends StatelessWidget {
     final TextTheme textTheme = Theme.of(context).textTheme;
     return Scaffold(
       appBar: AppBar(
-        title: Text(Util.appNameMM),
+        title: InkWell(
+          onLongPress: () => _login(context),
+          child: Text(Util.appNameMM),
+        ),
         titleTextStyle: textTheme.labelLarge?.copyWith(fontSize: 18),
         actions: [
           IconButton(
@@ -42,8 +46,15 @@ class MainScreen extends StatelessWidget {
               ],
             ),
           ),
+          const SliverToBoxAdapter(child: SizedBox(height: 16)),
           SliverToBoxAdapter(
-            child: ,
+            child: Container(
+              height: 200,
+              color: Colors.blue,
+              child: const Center(
+                child: Text('PageView'),
+              ),
+            ),
           ),
           const SliverToBoxAdapter(child: SizedBox(height: 16)),
           SliverGrid.count(
@@ -55,26 +66,43 @@ class MainScreen extends StatelessWidget {
                 width: 200,
                 height: 200,
                 color: Colors.red,
+                child: const Center(
+                  child: Text('MPT'),
+                ),
               ),
               Container(
                 width: 200,
                 height: 200,
                 color: Colors.green,
+                child: const Center(
+                  child: Text('ATOM'),
+                ),
               ),
               Container(
                 width: 200,
                 height: 200,
                 color: Colors.blue,
+                child: const Center(
+                  child: Text('MyTEL'),
+                ),
               ),
               Container(
                 width: 200,
                 height: 200,
                 color: Colors.black38,
+                child: const Center(
+                  child: Text('Oredoo'),
+                ),
               ),
             ],
           ),
         ],
       ),
     );
+  }
+
+  void _login(BuildContext context) {
+    // TODO: goto login screen
+    context.goNamed('login');
   }
 }
