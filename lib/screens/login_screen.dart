f69ee06b578867c4e25eb5074d83f37838e2d483
clicker <clickernux@gmail.com>
2023-10-11 23:04:36 +0630

    Implemented Form widget and firebase login logic.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,17 +10,139 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  late final TextEditingController _emailController;
+  late final TextEditingController _passwordController;
+
+  bool _showPassword = false;
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Admin Panel'),
+        title: const Text('Login Screen'),
       ),
       body: _buildBody(),
     );
   }
 
   Widget _buildBody() {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+              child: _isLoading
+                  ? const CircularProgressIndicator()
+                  : const Placeholder(),
+            ),
+            const SizedBox(height: 8),
+            TextFormField(
+              controller: _emailController,
+              autofillHints: const [
+                '199phoneservice@gmail.com',
+                'clickernux@gmail.com',
+              ],
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                // helperText: 'yourname@gmail.com',
+                label: Text('Email'),
+                icon: Icon(Icons.email),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please! Provides an email';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 8),
+            TextFormField(
+              controller: _passwordController,
+              keyboardType: TextInputType.visiblePassword,
+              obscureText: !_showPassword,
+              decoration: const InputDecoration(
+                label: Text('Password'),
+                icon: Icon(Icons.key),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Password must not be empty';
+                }
+                return null;
+              },
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Checkbox(value: _showPassword, onChanged: _onChanged),
+                const Text('Show Password'),
+                const Spacer(),
+              ],
+            ),
+            const SizedBox(height: 8),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  setState(() {
+                    _isLoading = true;
+                  });
+                  FirebaseAuth.instance
+                      .signInWithEmailAndPassword(
+                          email: _emailController.text,
+                          password: _passwordController.text)
+                      .then(
+                    (value) {
+                      debugPrint(value.user?.email);
+                      context.goNamed('admin_panel');
+                    },
+                  ).whenComplete(() {
+                    setState(() {
+                      _isLoading = false;
+                    });
+                  }).onError((error, stackTrace) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          error.toString(),
+                        ),
+                      ),
+                    );
+                  });
+                }
+              },
+              child: const Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onChanged(bool? value) {
+    setState(() {
+      _showPassword = value ?? false;
+    });
   }
 }
