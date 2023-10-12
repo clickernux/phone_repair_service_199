687c2192020e1adae9c976f4042720c52aa79eb8
clicker <clickernux@gmail.com>
2023-10-12 11:51:10 +0630

    Created slide_data.dart

---
+++ lib/model/slide_data.dart
@@ -0,0 +1,7 @@
+class SlideData {
+  final String title;
+  final String content;
+  final List<String> imgUrlList;
+
+  SlideData({this.title = '', this.content = '', this.imgUrlList = const []});
+}
