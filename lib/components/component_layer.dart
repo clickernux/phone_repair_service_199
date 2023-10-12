17524baf01f2d01968c5e4d894d21bdf6de50553
clicker <clickernux@gmail.com>
2023-10-12 21:12:13 +0630

    Created page_slide_view.dart

--- lib/components/component_layer.dart
+++ lib/components/component_layer.dart
@@ -1,2 +1,3 @@
 export 'banner_scroll.dart';
 export 'banner_card.dart';
+export 'page_slide_view.dart';
---
+++ lib/components/page_slide_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PageSlideView extends StatefulWidget {
+  const PageSlideView({super.key});
+
+  @override
+  State<PageSlideView> createState() => _PageSlideViewState();
+}
+
+class _PageSlideViewState extends State<PageSlideView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
