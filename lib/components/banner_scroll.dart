c4412cf8725fa1e5498fc420bcec8a9290d4f22c
clicker <clickernux@gmail.com>
2023-10-16 22:14:49 +0630

    Used AnimatedSwitcher to show info.

--- lib/components/banner_scroll.dart
+++ lib/components/banner_scroll.dart
@@ -1,16 +1,41 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:phone_repair_service_199/util.dart';
 
 import 'component_layer.dart';
 
-class BannerScroll extends StatelessWidget {
+class BannerScroll extends StatefulWidget {
   const BannerScroll({super.key});
+
+  @override
+  State<BannerScroll> createState() => _BannerScrollState();
+}
+
+class _BannerScrollState extends State<BannerScroll> {
+  late final Timer _timer;
+  int index = 0;
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(const Duration(seconds: 7), (timer) {
+      setState(() {
+        index = (index + 1) % Util.bannerContent.length;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 280,
-      child: _buildListView(context),
+      child: _introInfo(context),
     );
   }
 
@@ -26,6 +51,39 @@ class BannerScroll extends StatelessWidget {
         final content = Util.bannerContent[index];
         return BannerCard(text: content.text, imgPath: content.imgPath);
       },
+    );
+  }
+
+  Widget _introInfo(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 12,
+        horizontal: 16,
+      ),
+      decoration: const BoxDecoration(
+        color: Colors.black87,
+      ),
+      alignment: Alignment.bottomLeft,
+      child: AnimatedSwitcher(
+        duration: const Duration(seconds: 1),
+        transitionBuilder: (child, animation) {
+          return FadeTransition(
+            opacity: animation,
+            child: child,
+          );
+        },
+        child: Text(
+          Util.bannerContent[index].text,
+          key: ValueKey<String>(
+            Util.bannerContent[index].text,
+          ),
+          style: textTheme.labelMedium?.copyWith(
+            color: Colors.white,
+            height: 1.8,
+          ),
+        ),
+      ),
     );
   }
 }
