9781e5095d74054da2b084e2247141f765755188
clicker <clickernux@gmail.com>
2023-10-29 17:16:55 +0630

    Added collectionNameMessage text field

--- lib/util.dart
+++ lib/util.dart
@@ -2,6 +2,7 @@ class Util {
   static String appNameMM = 'ဝမ်းနိုင်းနိုင်း ဖုန်းပြုပြင်ရေး';
   static String appNameEng = '199 Phone Repair Service';
   static String collectionName = 'slide-data';
+  static String collectionNameMessage = 'messages';
 
   static List<({String text, String imgPath})> bannerContent = [
     (
