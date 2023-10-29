e00a7a04db4d90d327f7c6b1377b473972e48c54
clicker <clickernux@gmail.com>
2023-10-29 22:00:15 +0630

    Implemented MessageListWidget.

--- lib/components/message_page.dart
+++ lib/components/message_page.dart
@@ -99,6 +99,8 @@ class _MessagePageState extends State<MessagePage> {
                   : const Icon(LineIcons.upload),
               label: const Text('Submit'),
             ),
+            const SizedBox(height: 16),
+            const MessageListWidget(),
           ],
         ),
       ),
@@ -130,6 +132,59 @@ class _MessagePageState extends State<MessagePage> {
       SnackBar(
         content: Text(msg),
       ),
+    );
+  }
+}
+
+class MessageListWidget extends StatelessWidget {
+  const MessageListWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final db = FirebaseFirestore.instance;
+    return StreamBuilder(
+      stream: db
+          .collection(Util.collectionNameMessage)
+          .orderBy('timestamp', descending: true)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (snapshot.hasData) {
+          final data = snapshot.data;
+          if (data == null || data.docs.isEmpty) {
+            return const SizedBox.shrink();
+          }
+          return Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Messages'),
+                const Divider(),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: data.docs.length,
+                    itemBuilder: (context, index) {
+                      final doc = data.docs[index];
+                      return ListTile(
+                        title: Text(doc['message']),
+                        leading: Text('${index + 1}.'),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+        return const SizedBox.shrink();
+      },
     );
   }
 }
