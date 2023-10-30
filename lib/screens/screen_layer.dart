e96e27310d947416fc78cdce0f7a8276c198c71f
clicker <clickernux@gmail.com>
2023-10-30 21:04:04 +0630

    Renamed notification_screen to activity_screen

--- lib/screens/screen_layer.dart
+++ lib/screens/screen_layer.dart
@@ -5,4 +5,4 @@ export 'about_screen.dart';
 export 'login_screen.dart';
 export 'admin_panel_screen.dart';
 export 'page_data_screen.dart';
-export 'notification_screen.dart';
+export 'activity_screen.dart';
