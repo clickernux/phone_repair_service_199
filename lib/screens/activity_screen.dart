f88c4827784bb6a9a8c0f5cf73d4a42024bc327d
clicker <clickernux@gmail.com>
2023-10-30 21:24:29 +0630

    Implemented swipe to delete item.

--- lib/screens/activity_screen.dart
+++ lib/screens/activity_screen.dart
@@ -4,14 +4,26 @@ import 'package:go_router/go_router.dart';
 import 'package:phone_repair_service_199/components/component_layer.dart';
 import 'package:phone_repair_service_199/util.dart';
 
-class NotificationScreen extends StatelessWidget {
+class NotificationScreen extends StatefulWidget {
   const NotificationScreen({super.key});
+
+  @override
+  State<NotificationScreen> createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Notifications'),
+        bottom: _isLoading
+            ? const PreferredSize(
+                preferredSize: Size.fromHeight(4.0),
+                child: LinearProgressIndicator())
+            : null,
       ),
       body: _buildBody(context),
     );
@@ -21,8 +33,10 @@ class NotificationScreen extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
       child: FutureBuilder(
-        future:
-            FirebaseFirestore.instance.collection(Util.collectionName).get(),
+        future: FirebaseFirestore.instance
+            .collection(Util.collectionName)
+            .orderBy('timestamp', descending: true)
+            .get(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(
@@ -48,16 +62,50 @@ class NotificationScreen extends StatelessWidget {
               final imgUrl = data.data()['imgList'] as List<dynamic>;
               return SizedBox(
                 height: 250,
-                child: InkWell(
-                  onTap: () {
-                    context.goNamed('noti', extra: data);
+                child: Dismissible(
+                  key: Key(data.id),
+                  direction: DismissDirection.endToStart,
+                  onDismissed: (_) {
+                    _deleteData(data.id);
                   },
-                  child: PageCard(imgUrl: imgUrl, doc: data),
+                  child: InkWell(
+                    onTap: () {
+                      context.goNamed('noti', extra: data);
+                    },
+                    child: PageCard(imgUrl: imgUrl, doc: data),
+                  ),
                 ),
               );
             },
           );
         },
+      ),
+    );
+  }
+
+  void _deleteData(String id) {
+    setState(() {
+      _isLoading = true;
+    });
+    FirebaseFirestore.instance
+        .collection(Util.collectionName)
+        .doc(id)
+        .delete()
+        .then((value) {
+      showSnackbarMsg('Item Deleted!');
+    }).onError((error, stackTrace) {
+      showSnackbarMsg(error.toString());
+    }).whenComplete(() {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
+
+  void showSnackbarMsg(String msg) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(msg),
       ),
     );
   }
