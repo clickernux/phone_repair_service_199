6b186abe1592a7dd808bc14fefa52ff60925ffe6
clicker <clickernux@gmail.com>
2023-11-07 20:46:20 +0630

    Removed redundant code.

--- lib/components/banner_scroll.dart
+++ lib/components/banner_scroll.dart
@@ -3,8 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:phone_repair_service_199/util.dart';
 
-import 'component_layer.dart';
-
 class BannerScroll extends StatefulWidget {
   const BannerScroll({super.key});
 
@@ -36,21 +34,6 @@ class _BannerScrollState extends State<BannerScroll> {
     return SizedBox(
       height: 250,
       child: _introInfo(context),
-    );
-  }
-
-  Widget _buildListView(BuildContext context) {
-    return ListView.builder(
-      scrollDirection: Axis.horizontal,
-      itemCount: Util.bannerContent.length + 1,
-      itemBuilder: (context, index) {
-        if (index >= Util.bannerContent.length) {
-          return TextButton(
-              onPressed: () {}, child: const Text('ပိုမိုသိရှိရန် >>'));
-        }
-        final content = Util.bannerContent[index];
-        return BannerCard(text: content.text, imgPath: content.imgPath);
-      },
     );
   }
 
