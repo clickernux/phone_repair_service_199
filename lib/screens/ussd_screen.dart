168c1638302fa09b1937c35f26c901445edeca4c
clicker <clickernux@gmail.com>
2023-11-07 21:18:49 +0630

    Implemented _buildBody method in ussd_screen.dart

--- lib/screens/ussd_screen.dart
+++ lib/screens/ussd_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:phone_repair_service_199/load_json.dart';
 
 class UssdScreen extends StatelessWidget {
   const UssdScreen({
@@ -13,7 +14,39 @@ class UssdScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(operatorName),
       ),
-      body: const Placeholder(),
+      body: _buildBody(context),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return FutureBuilder(
+      future: LoadJson.ussdCodes(operatorName),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasError) {
+          return Center(
+            child: Text(snapshot.error.toString()),
+          );
+        }
+        final data = snapshot.data;
+        if (data == null || data.isEmpty) {
+          return const Center(child: Text('No Data'));
+        }
+
+        return ListView.builder(
+          itemCount: data.length,
+          primary: true,
+          itemBuilder: (context, index) {
+            final code = data[index];
+            return ListTile(
+              title: Text(code.service),
+              subtitle: Text(code.code),
+            );
+          },
+        );
+      },
     );
   }
 }
