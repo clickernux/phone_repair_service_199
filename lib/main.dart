3302d584ec96fd491d1fd7fb202fe4d1eeec2fb7
clicker <clickernux@gmail.com>
2023-11-14 21:21:16 +0630

    load dotenv file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:phone_repair_service_199/firebase_options.dart';
 import 'package:phone_repair_service_199/main_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await dotenv.load(fileName: ".env");
   runApp(const PhoneRepairService199App());
 }
 
