b7e0b57502a69d5d0934cf316762143483e0141d
clicker <clickernux@gmail.com>
2023-11-14 21:39:11 +0630

    Initialized hive database.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:phone_repair_service_199/firebase_options.dart';
 import 'package:phone_repair_service_199/main_router.dart';
 
@@ -8,6 +9,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await dotenv.load(fileName: ".env");
+  await Hive.initFlutter();
   runApp(const PhoneRepairService199App());
 }
 
