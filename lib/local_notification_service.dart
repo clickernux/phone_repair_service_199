74f4da107b4de5d93a9c229047ec346f1eca35bf
clicker <clickernux@gmail.com>
2023-11-19 20:45:10 +0630

    Created & implemented local_notification_service

---
+++ lib/local_notification_service.dart
@@ -0,0 +1,47 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotificationService {
+  static final FlutterLocalNotificationsPlugin _notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  static Future<void> initialize() async {
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: AndroidInitializationSettings('ic_launcher'),
+    );
+
+    await _notificationsPlugin.initialize(initializationSettings,
+        onDidReceiveNotificationResponse: onDidReceiveNotificationResponse);
+  }
+
+  static Future<void> display({
+    required String title,
+    required String message,
+    required String channelId,
+    required String channelName,
+    required String channelDesc,
+    required Importance importance,
+    required Priority priority,
+  }) async {
+    final AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails(channelId, channelName,
+            channelDescription: channelDesc,
+            importance: importance,
+            priority: priority,
+            styleInformation: BigTextStyleInformation(message));
+
+    final NotificationDetails notificationDetails =
+        NotificationDetails(android: androidNotificationDetails);
+
+    await _notificationsPlugin.show(0, title, message, notificationDetails);
+  }
+
+  static void onDidReceiveNotificationResponse(
+      NotificationResponse details) async {
+    final String? payload = details.payload;
+
+    if (details.payload != null) {
+      print('notification payload: $payload');
+    }
+  }
+}
