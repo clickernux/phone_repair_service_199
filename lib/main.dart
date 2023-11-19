6bc491a647aa7a54dbaa3cf220ada4540db28070
clicker <clickernux@gmail.com>
2023-11-19 21:14:27 +0630

    Show notification when background task has done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,27 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:phone_repair_service_199/fetch_blog.dart';
 import 'package:phone_repair_service_199/firebase_options.dart';
+import 'package:phone_repair_service_199/local_notification_service.dart';
 import 'package:phone_repair_service_199/main_router.dart';
 import 'package:phone_repair_service_199/util.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:workmanager/workmanager.dart';
+
+const String channelId = 'fetchBlog';
+const String channelName = 'FetchingBlog';
+const String channelDesc =
+    'This channel is used for showing fetching blog posts message';
 
 void callbackDispatcher() {
   Workmanager().executeTask((taskName, inputData) async {
     debugPrint('Native called background task: $taskName');
     await dotenv.load(fileName: ".env");
     await Hive.initFlutter();
+    LocalNotificationService.initialize();
     try {
       final apiKey = dotenv.env['BLOGGER_API'] ?? '';
       final blogId = dotenv.env['BLOG_ID'] ?? '';
@@ -22,9 +31,27 @@ void callbackDispatcher() {
       debugPrint('Total Posts: ${posts.length}');
 
       final box = await Hive.openBox(Util.bloggerPost);
+      final previousPostCount = box.length;
+      final deletedPostCount = await box.clear();
+
+      debugPrint('Deleted $deletedPostCount posts!');
 
       for (var post in posts) {
         box.put(post.id, post.toJson());
+      }
+      if (box.length != previousPostCount) {
+        LocalNotificationService.display(
+          title: 'ဖတ်စရာအသစ်ရပါပြီ',
+          message:
+              'သင့်အတွက် ဖတ်ရှုစရာအကြောင်းအရာများ ${box.length}ခု ရရှိထားပါသည်။',
+          channelId: channelId,
+          channelName: channelName,
+          channelDesc: channelDesc,
+          importance: Importance.max,
+          priority: Priority.max,
+        );
+      } else {
+        debugPrint('Notification Skip Showing!');
       }
     } catch (error) {
       throw Exception(error);
@@ -37,13 +64,26 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await Hive.initFlutter();
+  final prefs = await SharedPreferences.getInstance();
+  final isFirstLaunch = prefs.getBool('isFirstLaunch') ?? true;
   Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
-  Workmanager().registerOneOffTask(
-    'task-identifier',
-    'FetchBloggerPost',
-    constraints: Constraints(
-      networkType: NetworkType.connected,
-    ),
+
+  if (isFirstLaunch) {
+    Workmanager().registerOneOffTask(
+      'first-launch-task',
+      'FetchBloggerPost',
+      constraints: Constraints(
+        networkType: NetworkType.connected,
+      ),
+    );
+    await prefs.setBool('isFirstLaunch', false);
+  }
+
+  Workmanager().registerPeriodicTask(
+    'periodic-task',
+    'fetch blog posts',
+    constraints: Constraints(networkType: NetworkType.connected),
+    frequency: const Duration(hours: 3),
   );
   runApp(const PhoneRepairService199App());
 }
