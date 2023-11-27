6383c1b6d1cbcfe5e7519fdc8398d3f646c90c69
clicker <clickernux@gmail.com>
2023-11-27 20:25:38 +0630

    Updated AboutScreen UI.

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../components/component_layer.dart';
 
@@ -13,7 +14,7 @@ class AboutScreen extends StatelessWidget {
         title: const Text('ဆက်သွယ်ရန်'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => _launchPhoneCall('09256343863'),
             icon: const Icon(LineIcons.phone),
           ),
         ],
@@ -24,30 +25,71 @@ class AboutScreen extends StatelessWidget {
 
   Widget _buildBody(BuildContext context) {
     return ListView(
-      children: const [
-        SizedBox(
+      children: [
+        const SizedBox(
           height: 280,
           child: Placeholder(),
         ),
-        InfoCard(
+        const InfoCard(
             title: '199 Phone Repair Service',
             desc:
                 'တန့်ယန်းမြို့တွင် ဖုန်းပြုပြင်ရေး သီးသန့်ဖြင့် နှစ်ပေါင်းများစွာ  ရပ်တည်ခဲ့ပြီးဖြစ်သည်။\nဖုန်းအရောင်းဆိုင်များမှ ကိုယ်တိုင်ပြုပြင်ရန်ခက်ခဲသည့် ဖုန်းများကို ဆိုင်ဈေးသီးသန့်ဖြင့် လက်ခံပြုပြင်ပေးနေသည်။'),
-        InfoCard(
+        const InfoCard(
           title: 'လိပ်စာ',
           desc:
               'ရပ်ကွက်(၃)၊ မြို့နယ်ခန်းမအနီး၊ ရွှင်ပျော်ပျော် စားသောက်ဆိုင်ကပ်လျက်၊ ဝမ်းနိုင်းနိုင်း ဖုန်းပြုပြင်ရေး၊ တန့်ယန်းမြို့။',
         ),
-        InfoCard(
+        const InfoCard(
           title: 'ဖုန်းနံပါတ်',
           desc: '09 256343863',
         ),
-        InfoCard(
-          title: 'Social Media',
+        const InfoCard(
+          title: 'ဆက်သွယ်ရန်',
           desc:
               'ဖုန်းနှင့်ပတ်သက်သော အကြောင်းအရာအများကို ဆက်သွယ်မေးမြန်းနိုင်ရန် အထက်ဖော်ပြပါ ဆိုင်ဖုန်းနံပါတ်နှင့် WeChat, Viber အကောင့်ဖွင့်ထားပါသည်။',
         ),
+        ListTile(
+          title: Text(
+            'Facebook Page',
+            style: Theme.of(context)
+                .textTheme
+                .bodyLarge
+                ?.copyWith(fontWeight: FontWeight.w500),
+          ),
+          subtitle: const Text('fb://phoneservice199'),
+          trailing: IconButton(
+              onPressed: () {
+                _launchFacebook('www.facebook.com/phoneservice199');
+              },
+              icon: const Icon(
+                LineIcons.link,
+                size: 32,
+              )),
+        ),
+        const SizedBox(height: 16),
       ],
     );
+  }
+
+  void _launchFacebook(String s) async {
+    final rawUrl = 'fb://facewebmodal/f?href=https://$s';
+    final url = Uri.parse(rawUrl);
+    if (!await canLaunchUrl(url)) {
+      await launchUrl(
+        url,
+        mode: LaunchMode.externalApplication,
+      );
+    } else {
+      debugPrint('Unable to launch $url');
+    }
+  }
+
+  void _launchPhoneCall(String s) async {
+    final uri = Uri.parse('tel:$s');
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      debugPrint('Unable to make phone call to $s');
+    }
   }
 }
