a1b6c716e4279a5712f6bca7622a74834909a2b8
clicker <clickernux@gmail.com>
2023-12-03 21:49:16 +0630

    Implemented background service that show message.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -11,51 +12,107 @@ import 'package:phone_repair_service_199/util.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:workmanager/workmanager.dart';
 
-const String channelId = 'fetchBlog';
-const String channelName = 'FetchingBlog';
-const String channelDesc =
+const String channelIdForBlog = 'fetchBlog';
+const String channelNameForBlog = 'FetchingBlog';
+const String channelDescForBlog =
     'This channel is used for showing fetching blog posts message';
+
+const String channelIdNoti = 'notification';
+const String channelNameNoti = 'Get Notification';
+const String channelDescNoti =
+    'This channel is used for showing messages send by Admin';
 
 void callbackDispatcher() {
   Workmanager().executeTask((taskName, inputData) async {
     debugPrint('Native called background task: $taskName');
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
     await dotenv.load(fileName: ".env");
     await Hive.initFlutter();
     LocalNotificationService.initialize();
-    try {
-      final apiKey = dotenv.env['BLOGGER_API'] ?? '';
-      final blogId = dotenv.env['BLOG_ID'] ?? '';
+    if (taskName == 'OneOffFetchBlogPosts' ||
+        taskName == 'PeriodicFetchBlogPosts') {
+      try {
+        final apiKey = dotenv.env['BLOGGER_API'] ?? '';
+        final blogId = dotenv.env['BLOG_ID'] ?? '';
 
-      final posts = await FetchBlog.fetchBlog(blogId, apiKey);
+        final posts = await FetchBlog.fetchBlog(blogId, apiKey);
 
-      debugPrint('Total Posts: ${posts.length}');
+        debugPrint('Total Posts: ${posts.length}');
 
-      final box = await Hive.openBox(Util.bloggerPost);
-      final previousPostCount = box.length;
-      final deletedPostCount = await box.clear();
+        final box = await Hive.openBox(Util.bloggerPost);
+        final previousPostCount = box.length;
+        final deletedPostCount = await box.clear();
 
-      debugPrint('Deleted $deletedPostCount posts!');
+        debugPrint('Deleted $deletedPostCount posts!');
 
-      for (var post in posts) {
-        await box.put(post.id, post.toJson());
+        for (var post in posts) {
+          await box.put(post.id, post.toJson());
+        }
+        if (box.length != previousPostCount) {
+          LocalNotificationService.display(
+            title: 'ဖတ်စရာအသစ်ရပါပြီ',
+            message:
+                'သင့်အတွက် ဖတ်ရှုစရာအကြောင်းအရာများ ${box.length - previousPostCount}ခု ရရှိထားပါသည်။',
+            channelId: channelIdForBlog,
+            channelName: channelNameForBlog,
+            channelDesc: channelDescForBlog,
+            importance: Importance.max,
+            priority: Priority.max,
+          );
+        } else {
+          debugPrint('Notification Skip Showing!');
+        }
+      } catch (error) {
+        throw Exception(error);
       }
-      if (box.length != previousPostCount) {
-        LocalNotificationService.display(
-          title: 'ဖတ်စရာအသစ်ရပါပြီ',
-          message:
-              'သင့်အတွက် ဖတ်ရှုစရာအကြောင်းအရာများ ${box.length - previousPostCount}ခု ရရှိထားပါသည်။',
-          channelId: channelId,
-          channelName: channelName,
-          channelDesc: channelDesc,
-          importance: Importance.max,
-          priority: Priority.max,
-        );
+    } else {
+      final data = await FirebaseFirestore.instance
+          .collection(Util.collectionNameMessage)
+          .orderBy('timestamp', descending: true)
+          .limit(1)
+          .get();
+      if (data.docs.isEmpty) {
+        debugPrint('No Notification!');
       } else {
-        debugPrint('Notification Skip Showing!');
+        final notiBox = await Hive.openBox(Util.notiBox);
+        final doc = data.docs.first;
+
+        // if there is no data in notiBox
+        // the doc will be saved in notiBox
+        // and will show the content as notification
+        if (notiBox.isEmpty) {
+          final String message = doc.data()['message'];
+          notiBox.put('message', message).then((value) {
+            LocalNotificationService.display(
+              title: 'New Message',
+              message: message,
+              channelId: channelIdNoti,
+              channelName: channelNameNoti,
+              channelDesc: channelDescNoti,
+              importance: Importance.max,
+              priority: Priority.max,
+            );
+          });
+        } else {
+          final String message = doc.data()['message'];
+          final String oldMessage = await notiBox.get('message');
+          if (message != oldMessage) {
+            await notiBox.put('message', message);
+            LocalNotificationService.display(
+              title: 'မင်္ဂလာပါ',
+              message: message,
+              channelId: channelIdNoti,
+              channelName: channelNameNoti,
+              channelDesc: channelDescNoti,
+              importance: Importance.max,
+              priority: Priority.max,
+            );
+          }
+        }
       }
-    } catch (error) {
-      throw Exception(error);
     }
+
     return true;
   });
 }
@@ -76,6 +133,14 @@ void main() async {
         networkType: NetworkType.connected,
       ),
     );
+
+    Workmanager().registerOneOffTask(
+      'first-launch-fetch-message',
+      'OneOffFetchingMessage',
+      constraints: Constraints(
+        networkType: NetworkType.connected,
+      ),
+    );
     sharePref.setBool('firstLaunch', false);
   }
 
@@ -84,6 +149,14 @@ void main() async {
     'PeriodicFetchBlogPosts',
     constraints: Constraints(networkType: NetworkType.connected),
     frequency: const Duration(hours: 5),
+  );
+  Workmanager().registerPeriodicTask(
+    'periodic-task-message',
+    'PeriodicFetchMessage',
+    constraints: Constraints(
+      networkType: NetworkType.connected,
+    ),
+    frequency: const Duration(hours: 1),
   );
   runApp(const PhoneRepairService199App());
 }
