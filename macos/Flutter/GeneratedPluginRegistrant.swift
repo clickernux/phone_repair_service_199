ad9b8890c713ab609b48a59ea0533b187f9d0cc8
clicker <clickernux@gmail.com>
2024-12-23 21:07:50 +0630

    updated pubspec

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,7 +15,7 @@ import isar_flutter_libs
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
